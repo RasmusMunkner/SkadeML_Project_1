a50c96e0d90cd0ad95db346af14ffa3d4713400a
HonghaoLiu1992 <denmark0045@gmail.com>
2023-02-28 20:42:43 +0100

    Honghao

--- HonghaoTest2.R
+++ HonghaoTest2.R
@@ -1,1 +1,18 @@
-print('HonghaoTest3')
+
+#Installing packages
+install.packages(c("xts", "sp", "zoo"))
+install.packages("CASdatasets", repos = "http://cas.uqam.ca/pub/", type="source")
+install.packages('gmodels')
+library(gmodels)
+
+#Load the package containing the dataset
+library(CASdatasets)
+?CASdatasets
+
+#Load the data into memory
+data("freMPL1")
+
+Freq1 <- glm(ClaimInd ~ DrivAge + offset(Exposure), data=freMPL1, family="binomial")
+summary(Freq1)
+
+Freq1 <- glm(ClaimInd ~ Gender, data=freMPL1, family="binomial")
